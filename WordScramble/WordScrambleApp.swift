f1743ee4fd73a3038e5396bb7017d837ae749d1a
ledoux25 <sanguojoseph17@gmail.com>
2025-09-03 20:20:43 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sanguo Joseph Ledoux on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
